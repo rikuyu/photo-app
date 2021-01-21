4faae56259e1709cb6b9eb964f34044d9e3e3557
rikuyu <rikuyu5884@gmail.com>
2021-01-21 15:35:06 +0900

    introduce typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,59 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
+type Photo = {
+  id: number;
+  urls: {
+    regular: string;
+  };
+  alt_description: string;
+};
+
+const App: React.FC<Photo> = () => {
   const [images, setImages] = useState([]);
-  const [text, setText] = useState("");
-  const [query, setQuery] = useState("apple");
+  const [text, setText] = useState<string>("");
+  const [query, setQuery] = useState<string>("cat");
 
   useEffect(() => {
-    console.log("useEffect was called");
     fetch(
       `https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        console.log(data.results);
         setImages(data.results);
       });
   }, [query]);
 
-  const onSubmit = (e) => {
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setQuery(text);
     setText("");
-    console.log("onSubmit was called");
   };
 
   return (
     <div className="App">
       <div className="main">
         <form onSubmit={onSubmit}>
+          <h1>Photo Search App</h1>
           <input
             type="text"
-            onChange={(e) => setText(e.target.value)}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setText(e.target.value)
+            }
             value={text}
           />
           <button type="submit">Search</button>
         </form>
       </div>
       <div className="container">
-        {images.map((image) => (
+        {images.map((image: Photo) => (
           <div key={image.id} className="card">
-            <img src={image.urls.regular} className="card-img" alt="" />
+            <img
+              src={image.urls.regular}
+              className="card-img"
+              alt={image.alt_description}
+            />
             <div className="card-content">
               <h1 className="card-title">{image.alt_description}</h1>
             </div>
@@ -49,6 +62,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
